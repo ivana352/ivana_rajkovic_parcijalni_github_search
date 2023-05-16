be86a554cfe1a8606f94a8ad5c9b9b2b04327286
Ivana Rajkovic <ivana352@gmail.com>
2023-05-16 17:21:07 +0200

    changes

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,29 +1,106 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./Home.css";
+import axios from "../../../axios";
+import User from "../ui/User";
 
 const Home = () => {
+    const [query, setQuery] = useState("");
+    // USERS FETCHED FROM API
+    const [users, setUsers] = useState([]);
+
+    //PAGE
+    const [page, setPage] = useState(1);
+    //PER PAGE
+    const [limit, setLimit] = useState(10);
+
+    const handleQueryInput = (e) => {
+        const value = e.target.value;
+        setQuery(value);
+    }
+
+    const handlePrevPage = () => {
+        setPage (page => {
+            if(page === 1) return page;
+            else return page -1;
+        })
+    }
+
+    const handleNextPage = () => {
+        setPage(page => page +1);
+    }
+
+    const handlePageLimit = (e) => {
+        const value = e.target.value;
+        setLimit(parseInt(value));
+      };
+
+    const fetchUsers = async () => {
+        try{
+        const {data} = await axios.get("/search/users?q=" + query)
+            return data?.items; 
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+    const handleSearchUsers = async (e) => {
+        e.preventDefault();
+            if (query) {
+               const items = await fetchUsers();
+               setUsers(items);
+            }
+            else{
+                console.log("empty...");
+            }
+    }
+
+    useEffect(() => {
+        const displayUsersOnChange = () => {
+            if(query) {
+                const items = await fetchUsers();
+                setUsers(items);
+            }
+        }
+        displayUsersOnChange();
+    }, [page, limit])
+
+
     return (
         <div className="container">
             <div className="search-form">
                 <h2>GitHub search user</h2>
                 <form>
-                    <input type="text" />
-                    <button>Search</button> 
+                    <input value={query} onChange={handleQueryInput} type="text" />
+                    <button onClick={handleSearchUsers}>
+                        Search
+                    </button> 
                 </form>
             </div>
 
             <div className="search-results">
-                <div className="user">
-                    <div className="image">
-                        <img src="https://avatars.githubusercontent.com/u/128634248?s=400&u=fb0f876c380d28ba792c7b92d967f42b77939487&v=4" />
-                    </div>
-
-                    <div className="user-info">
-                        <h3>User name:</h3>
-                        <small>ID34fj</small>
-                        <a href="">View profile</a>
+                <div className="more-options">
+                    <label>
+                        <small>Per page: </small>
+                        <select onChange={handlePageLimit} >
+                            <option value="10">10</option>
+                            <option value="20">20</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+                        </select>
+                    </label>
+                    <div className="pagination">
+                        <button onClick={handlePrevPage}> {page} </button>
+                        <button onClick={handleNextPage}> {page + 1} </button>
                     </div>
                 </div>
+                {users ? (
+                    users.map((user) => {
+                        return <User user = {user} key={user.id} /> 
+                        })
+                    ) : (
+                    <h2>Nothing to display...</h2>
+                )}
             </div>
         </div>
     )
