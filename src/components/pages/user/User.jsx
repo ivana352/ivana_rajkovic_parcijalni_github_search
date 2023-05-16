ea622dd077163dfd5e8237f0eb5243f71fa87c3a
Ivana Rajkovic <ivana352@gmail.com>
2023-05-16 19:24:05 +0200

    changes

--- src/components/pages/user/User.jsx
+++ src/components/pages/user/User.jsx
@@ -6,6 +6,7 @@ import information from "../../../assets/information.png"
 import github from "../../../assets/github.png"
 import { Link, useParams } from "react-router-dom";
 import axios from "../../../axios";
+import Repo from "../ui/Repo";
 // import Repo from "../../ui/Repo";
 
 const User = () => {
@@ -44,33 +45,41 @@ const User = () => {
             <div className="user-information">
 
                 <div className="image">
-                    <img src="https://avatars.githubusercontent.com/u/128634248?s=400&u=fb0f876c380d28ba792c7b92d967f42b77939487&v=4" />
+                    <img src={userInfo?.avatar_url} />
                 </div>
 
                 <div className="user-content">
 
-                    <h3>Name of the user:</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo reprehenderit unde tempora illo, dicta similique placeat nobis laborum voluptatibus nostrum molestias ab est sit expedita accusantium cum deserunt facere non?</p>
+                    <h3>{userInfo?.name}</h3>
+
+                    <p>
+                        {userInfo?.bio}
+                    </p>
+
                     <div className="more-data">
 
                         <p> 
                             <img src={user} alt="user icon"/> 
-                            User name: 
+                            {userInfo?.followers} Followers. Following {userInfo?.following}
                         </p>
 
-                        <p> 
-                            <img src={location} alt="location icon"/> 
-                            Location: 
-                        </p>
-
-                        <p> 
-                            <img src={information} alt="info icon"/> 
-                            User info: 
-                        </p>
+                        {userInfo?.location && <p>
+                            <img src={location} 
+                                 alt="location icon" 
+                            />
+                            {userInfo?.location}
+                        </p>}
+                            
+                        {userInfo?.blog && <p>
+                            <img src={user} 
+                                 alt=""
+                            />
+                            {userInfo?.blog}
+                        </p>}
 
                         <p> 
                             <img src={github} alt="github icon"/> 
-                            <a href="#"> View github profile: </a>
+                            <a href={userInfo?.html_url}> View github profile: </a>
                         </p>
 
                     </div>
@@ -78,14 +87,11 @@ const User = () => {
             </div>
 
             <div className="user-repos">
-                <div className="repo">
-                    <h3>
-                        <a href="#"> Name of the repository: </a>
-                    </h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quis at totam et. Laborum ullam quo repellat doloribus. Nulla maiores ipsam autem sequi consectetur, vitae possimus? Eos vel nemo deserunt!</p>
-                    <br />
-                    <small>Written in JavaScript</small>
-                </div>
+                {
+                    repos ? repos.map(repo => {
+                        return <Repo repo={repo} key={repo.id} />
+                    }) : <h2>No repositories from this user.</h2>
+                }
             </div>
         </div>
     )
