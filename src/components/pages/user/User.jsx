0d5596c91978c7956ac307ecb7c2d6155cab8523
Ivana Rajkovic <ivana352@gmail.com>
2023-05-16 18:44:40 +0200

    useEffect hook

--- src/components/pages/user/User.jsx
+++ src/components/pages/user/User.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import "./User.css"
 import user from "../../../assets/user.png"
 import location from "../../../assets/location.png"
 import information from "../../../assets/information.png"
 import github from "../../../assets/github.png"
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import axios from "../../../axios";
+// import Repo from "../../ui/Repo";
 
 const User = () => {
+
+    const{login} = useParams();
+
+    //USER INFORMATION
+    const [userInfo, setUserInfo] = useState({});
+    //USER REPOSITORIES
+    const [repos, setRepos] = useState([]);
+
+    useEffect(() => {
+        const fetchUserInformation = async () => {
+            //   const responseUser = await axios.get("/userinfo");
+            //   const responseRepos = await axios.get("/repos");
+            try {
+                const response = await Promise.all([
+                    axios.get(`/users/${login}`),
+                    axios.get(`/users/${login}/repos`)
+                ]);
+                setUserInfo(response[0].data)
+                setRepos(response[1].data)
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchUserInformation()
+    });
+
+
     return(
         <div className="container">
                 <Link to="/" className="back"> 
