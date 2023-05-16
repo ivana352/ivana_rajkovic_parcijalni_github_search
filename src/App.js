7095953373daed99f1ce6d56959d57c8179ef111
Ivana Rajkovic <ivana352@gmail.com>
2023-05-16 17:52:02 +0200

    router

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={ <Home /> }/>
-          <Route path='/user' element={ <User />}/>
+          <Route path='/user/:login' element={ <User />}/>
         </Routes>
       </BrowserRouter>
     </div>
